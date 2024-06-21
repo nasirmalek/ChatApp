b55a916844d92dfd0c602a86d2b6f3d8c7f51ef4
Nasir Malek <113028970+nasirmalek@users.noreply.github.com>
2024-06-21 19:37:50 +0530

    Update settings.py

--- chat-app/djangochat/djangochat/settings.py
+++ chat-app/djangochat/djangochat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_c_smsxpu!k$l*+o3*ohkh3eai&g&gn15z^=)r=k!y_b3k01#*'
+SECRET_KEY = 'YOUR_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
